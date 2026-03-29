88d42d100e05ad5022610e2a03502097d6cb8425
paul thibault <107209631+Vidocq0108@users.noreply.github.com>
2026-03-29 19:30:46 +0200

    Update WeightConverterFunction.py

--- WeightConverterFunction.py
+++ WeightConverterFunction.py
@@ -1,22 +1,23 @@
 class Weight:
     def __init__(self):
         self.tab = {
-            "t": 1_000_000,
-            "kg": 1000,
-            "hg": 100,
-            "dag": 10,
-            "g": 1,
-            "mg": 0.001,
-            "µg": 0.000001,
-            "oz": 28.3495,
-            "lb": 453.592,
-            "st": 6350.29,
-            "shortton_US": 907_185,
-            "longton_UK": 1_016_050,
-            "ct": 0.2,
-            "Da": 1.66054e-24,
-            "u": 1.66054e-24
+        "t": 1_000_000,      # tonne
+        "kg": 1000,          # kilogramme
+        "hg": 100,           # hectogramme
+        "dag": 10,           # décagramme
+        "g": 1,              # gramme
+        "mg": 0.001,         # milligramme
+        "µg": 0.000001,      # microgramme
+        "oz": 28.3495,       # once (ounce)
+        "lb": 453.592,       # livre (pound)
+        "st": 6350.29,       # stone
+        "shortton_US": 907_185,   # tonne courte américaine (short ton US)
+        "longton_UK": 1_016_050,  # tonne longue britannique (long ton UK)
+        "ct": 0.2,           # carat
+        "Da": 1.66054e-24,   # dalton (unité de masse atomique)
+        "u": 1.66054e-24     # unité de masse atomique (unified atomic mass unit)
         }
+
 
     def convert_to_g(self, unit, value):
         return value * self.tab[unit]
